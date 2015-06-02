3608b34348a1009f0fd665f5fb918249aad48547
Samuel <samuel.p991@gmail.com>
2015-06-02 23:43:36 +0200

    Ready for testing

--- src/summarizer_test.cc
+++ src/summarizer_test.cc
@@ -1,5 +1,6 @@
 #include "freeling/morfo/analyzer.h"
 #include "freeling/morfo/util.h"
+#include "freeling/output/input_conll.h"
 
 #include "config.h"
 #include "summarizer.h"
@@ -24,37 +25,32 @@ int main (int argc, char **argv) {
   if (argc < 2) ipath = L"/usr/local";
   else ipath = util::string2wstring(argv[1]);
 
-  /// set config options (which modules to create, with which configuration)
-  analyzer::config_options cfg = fill_config(ipath+L"/share/freeling/");
-  /// create analyzer
-  analyzer anlz(cfg);
-
-  senses * sens = new senses(cfg.SENSE_ConfigFile);
-  ukb * dsb = new ukb(cfg.UKB_ConfigFile);
-
-  /// set invoke options (which modules to use)
-  analyzer::invoke_options ivk = fill_invoke();
-  /// load invoke options into analyzer
-  anlz.set_current_invoke_options(ivk);
-
-  /// load document to analyze
-  wstring text;  
   wstring line;
-  while (getline(wcin,line))
-    text = text + line + L"\n";
+  wifstream text_file("/home/samuel/Summarizer/src/pp.txt");
+  wstring text = L"";
+  while (getline(text_file, line))
+      text += line + L"\n";
 
-  /// analyze text, leave result in doc
+  wifstream analyzed_text_file("/home/samuel/Summarizer/src/pp.out");
+  
+  getline(analyzed_text_file, line);
+  wstring an_text = line;
+  while (getline(analyzed_text_file, line))
+      an_text += L"\n" + line;
+
+  wcout << text;
+
+  wcout << L"----" << endl;
+
+  wcout << an_text;
+
+  input_conll ip;
+
   document doc;
-  anlz.analyze(text,doc,true);
-
-  //sens->analyze(doc);
-  //dsb->analyze(doc);
-
+  ip.input_document(an_text,doc);
   
   for (list<paragraph>::const_iterator it_p = doc.begin(); it_p != doc.end(); it_p++) {
-    int j = 0;
     for (list<sentence>::const_iterator it_s = it_p->begin(); it_s != it_p->end(); it_s++) {
-      int k = 0;
       for (list<word>::const_iterator it_w = it_s->begin(); it_w != it_s->end(); it_w++) {
         wcout << it_w->get_form() << L" -> " << it_w->get_tag() << endl; 
       }
